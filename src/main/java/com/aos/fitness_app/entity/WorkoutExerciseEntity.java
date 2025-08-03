6cdf67be23dfe2dea17ccaee18d83b4ba461d2cd
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-04 00:42:15 +0300

    workout exercise entity, initial

---
+++ src/main/java/com/aos/fitness_app/entity/WorkoutExerciseEntity.java
@@ -0,0 +1,23 @@
+package com.aos.fitness_app.entity;
+
+
+import com.aos.fitness_app.common.enums.WorkoutDifficultyLevel;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "workouts")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class WorkoutExerciseEntity {
+
+
+
+}
