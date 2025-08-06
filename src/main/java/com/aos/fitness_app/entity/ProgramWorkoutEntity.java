79e4c949f4ef217bf6f6d8030534d69518901612
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-06 16:39:02 +0300

    program workout junction entity

---
+++ src/main/java/com/aos/fitness_app/entity/ProgramWorkoutEntity.java
@@ -0,0 +1,42 @@
+package com.aos.fitness_app.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "program_workout")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProgramWorkoutEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Many-to-one relationship with Program
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "program_id", nullable = false)
+    private ProgramEntity program;
+
+    // Many-to-one relationship with Workout
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "workout_id", nullable = false)
+    private WorkoutEntity workout;
+
+    @Column(name = "workout_order")
+    private Integer order;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+    }
+
+}
