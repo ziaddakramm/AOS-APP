f4f3f0acb878c0b33b0d988e55838fafd6b1f8b5
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-09 18:14:07 +0300

    user and email services for auth

---
+++ src/main/java/com/aos/fitness_app/auth/service/ApplicationUserService.java
@@ -0,0 +1,34 @@
+package com.aos.fitness_app.auth.service;
+
+import com.aos.fitness_app.auth.entity.ApplicationUser;
+import com.aos.fitness_app.auth.repository.ApplicationUserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+@RequiredArgsConstructor
+public class ApplicationUserService {
+
+    private final ApplicationUserRepository applicationUserRepository;
+    private final PasswordEncoder passwordEncoder;
+    public void updatePassword(ApplicationUser user, String newPassword) {
+        user.setPassword(passwordEncoder.encode(newPassword));
+        applicationUserRepository.save(user);
+    }
+
+    public void updatePasswordByEmail(String email, String newPassword) {
+        Optional<ApplicationUser> user = applicationUserRepository.findByEmail(email);
+        if(user.isPresent()){
+            updatePassword(user.get(), newPassword);
+        }else{
+            throw new RuntimeException("The use with email: " + email + " doesn't exist!");
+        }
+    }
+
+
+}
