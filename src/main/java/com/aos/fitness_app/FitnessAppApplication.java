face1c0523864fbce926269cf41af432d223e126
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-09 18:16:54 +0300

    enable scheduling annotation

--- src/main/java/com/aos/fitness_app/FitnessAppApplication.java
+++ src/main/java/com/aos/fitness_app/FitnessAppApplication.java
@@ -2,8 +2,10 @@ package com.aos.fitness_app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class FitnessAppApplication {
 
 	public static void main(String[] args) {
