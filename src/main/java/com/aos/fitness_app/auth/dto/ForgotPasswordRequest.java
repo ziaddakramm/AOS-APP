817729645850915a544526f0cd7cabb866029bd8
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-11 19:23:21 +0300

    forgot password request

---
+++ src/main/java/com/aos/fitness_app/auth/dto/ForgotPasswordRequest.java
@@ -0,0 +1,21 @@
+package com.aos.fitness_app.auth.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ForgotPasswordRequest {
+
+    @NotBlank(message = "Email is required")
+    @Email(message = "Please provide a valid email address")
+    @JsonProperty("email")
+    private String email;
+}
