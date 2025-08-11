ffc30c1b5c34a92506e104af7dc508dca2bb674b
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-11 19:27:37 +0300

    reset password request

--- src/main/java/com/aos/fitness_app/auth/dto/ResetPasswordRequest.java
+++ src/main/java/com/aos/fitness_app/auth/dto/ResetPasswordRequest.java
@@ -5,12 +5,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Builder
 public class ResetPasswordRequest {
 
     @NotBlank
