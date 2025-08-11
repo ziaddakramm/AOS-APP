25d9eb246be5f3c10a9049437c0384364de45029
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-11 19:23:05 +0300

    constants and utilities

---
+++ src/main/java/com/aos/fitness_app/app/common/Constants.java
@@ -0,0 +1,9 @@
+package com.aos.fitness_app.app.common;
+
+public final class Constants {
+    public static final String EMAIL_REGEX =
+            "^(?=.{1,64}@)[A-Za-z0-9_-]+(\\.[A-Za-z0-9_-]+)*@[^-][A-Za-z0-9-]+(\\.[A-Za-z0-9-]+)*(\\.[A-Za-z]{2,})$";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/aos/fitness_app/app/common/Utilities.java
@@ -0,0 +1,15 @@
+package com.aos.fitness_app.app.common;
+
+import java.util.regex.Pattern;
+
+public final class Utilities {
+
+    private Utilities() {
+    }
+
+    public static boolean patternMatches(String emailAddress, String regexPattern) {
+        return Pattern.compile(regexPattern)
+                .matcher(emailAddress)
+                .matches();
+    }
+}
