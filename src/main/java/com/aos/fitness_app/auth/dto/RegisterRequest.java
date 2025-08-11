a1c22040123089f8c66658b2730157a68f4b053f
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-11 19:27:24 +0300

    @Email annotation

--- src/main/java/com/aos/fitness_app/auth/dto/RegisterRequest.java
+++ src/main/java/com/aos/fitness_app/auth/dto/RegisterRequest.java
@@ -1,6 +1,7 @@
 package com.aos.fitness_app.auth.dto;
 
 import com.aos.fitness_app.auth.enums.Role;
+import jakarta.validation.constraints.Email;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -12,6 +13,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class RegisterRequest {
 
+  @Email
   private String email;
   private String password;
   private Role role;
