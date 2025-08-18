5966c1797676156485f28bf0ae9295eb09b93b74
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-19 01:40:47 +0300

    @valid annotation

--- src/main/java/com/aos/fitness_app/auth/controller/AuthenticationController.java
+++ src/main/java/com/aos/fitness_app/auth/controller/AuthenticationController.java
@@ -24,7 +24,7 @@ public class AuthenticationController {
 
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
-            @RequestBody RegisterRequest request
+           @Valid @RequestBody RegisterRequest request
     ) {
         authenticationService.validateEmail(request.getEmail());
         return ResponseEntity.ok(authenticationService.register(request));
