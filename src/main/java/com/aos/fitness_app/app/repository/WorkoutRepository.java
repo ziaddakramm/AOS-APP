f5cbe4b9e00fb18da4b056f6d78e6fcc9b06ab0e
ziadakramm <ziadwaterpolo12@gmail.com>
2025-08-19 01:43:37 +0300

    exists by name query

--- src/main/java/com/aos/fitness_app/app/repository/WorkoutRepository.java
+++ src/main/java/com/aos/fitness_app/app/repository/WorkoutRepository.java
@@ -3,9 +3,15 @@ package com.aos.fitness_app.app.repository;
 
 import com.aos.fitness_app.app.entity.WorkoutEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
 @Repository
 public interface WorkoutRepository extends JpaRepository<WorkoutEntity, Long> {
 
+    public boolean existsByName(String name);
+
+    @Query("SELECT COUNT(w) > 0 FROM WorkoutEntity w WHERE w.name = :name AND w.id != :excludeId")
+    boolean existsByNameExcludingId(@Param("name") String name, @Param("excludeId") Long excludeId);
 }
